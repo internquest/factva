877f50977817ea2c64a233fb2061c71f682af3a8
shiva kumar <98257507+Shivuduu19@users.noreply.github.com>
2024-11-11 22:39:03 +0530

    add how works

--- app/components/Howitworks.jsx
+++ app/components/Howitworks.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 
+
 const Howitworks = () => {
     return (
-        <div>
+        <div className='flex flex-col items-center justify-center pt-[100px] px-[30px] pb-[50px] relative w-full'>
+            <div className='flex flex-col justify-center items-center w-full gap-6'>
 
+                <h1 className='text-primary font-bold text-[48px] leading-[1.7em] tracking-[0px] uppercase'>HOW IT WORKS</h1>
+                <p className='text-primary font-normal text-[20px] leading-[1.9em] max-w-[696px] text-center tracking-[0px] '>
+                    Personify facilitates 1:1 conversations about your goals, your ambitions, your
+                    values, & how you see the world. No trick questions. No awkward small talk.
+                    Just a chance for you to be you.
+                </p>
+            </div>
+            <div>
+
+
+                <div className='bg-[#CDEF5C] w-[700px] h-min py-12 px-5 mt-10 flex gap-5 flex-col justify-center items-center'>
+                    <div className='bg-[var(--primary-color)] font-semibold w-[52px] h-[52px] text-[32px] text-[#f5f5f5] flex justify-center items-center rounded-[50%] '>
+                        1
+                    </div>
+
+                    <h2 className='text-primary text-[32px] mt-2 leading-[1.2em] tracking-normal font-semibold'>SIGN UP & SCHEDULE</h2>
+                    <div>
+
+                        <p className='text-primary text-[18px] max-w-[517px] teba leading-[1.9em] w-full text-center tracking-[0px] font-normal'>Create a profile and schedule your conversations with us.</p>
+                        <p className='text-primary text-[18px] max-w-[517px] teba leading-[1.9em] w-full text-center tracking-[0px] font-normal'>   It’s as simple as signing up for a coffee chat.</p>
+                    </div>
+                </div>
+                <div className='bg-[#0f0f0f] w-[700px] h-min py-12 px-5 mt-10 flex gap-5 flex-col justify-center items-center'>
+                    <div className='bg-[#FFFFFF] w-[52px] h-[52px] font-semibold text-[32px] text-[#0f0f0f] flex justify-center items-center rounded-[50%] '>
+                        2
+                    </div>
+
+                    <h2 className='text-white text-[32px] mt-2 leading-[1.2em] tracking-normal font-semibold'>JOIN THE CONVERSATION</h2>
+                    <div>
+
+                        <p className='text-white text-[18px] max-w-[543px] px-8 leading-[1.9em] w-full text-center tracking-[0px] font-normal'>Have a real, human conversation. You’ll be asked questions
+                            that matter, from your thoughts on leadership to how you
+                            handle challenges, to what motivates you & more.
+                        </p>
+
+                        <p className='text-white text-[18px] max-w-[543px]  leading-[1.9em] w-full text-center tracking-[0px] font-normal'>(Don’t worry, there are no wrong answers here.)</p>
+
+                    </div>
+                </div>
+                <div className='bg-[#F7F6F2] w-[700px] h-min py-12 px-5 mt-10 flex gap-5 flex-col justify-center items-center'>
+                    <div className='bg-[#3D3D3C] w-[52px] h-[52px] font-semibold text-[32px] text-[#F5F5F5] flex justify-center items-center rounded-[50%] '>
+                        3
+                    </div>
+
+                    <h2 className='text-[#3d3d3c] text-[32px] mt-2 leading-[1.2em] tracking-normal font-semibold'>GET YOUR PERSONALITY PROFILE</h2>
+                    <div>
+
+                        <p className='text-[#3d3d3c] text-[18px] px-8 max-w-[543px]  leading-[1.9em] w-full text-center tracking-[0px] font-normal'>A personalized profile shall be created based on your
+                            conversations. This profile will help companies see more
+                            than just your credentials—it’s a glimpse into the real you.
+                        </p>
+
+
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
